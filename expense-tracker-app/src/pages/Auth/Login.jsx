491423fb08212b057cc57164ad178e17e9927d45
Yun <monica.yunln@gmail.com>
2025-05-26 00:14:43 -0600

    Diseño login y signUp

--- expense-tracker-app/src/pages/Auth/Login.jsx
+++ expense-tracker-app/src/pages/Auth/Login.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import {Link, useNavigate} from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if(!validateEmail(email)) {
+      setError("Ingrese un correo valido");
+      return;
+    }
+
+    if(!password){
+      setError("Ingrese una contraseña valida");
+      return;
+    }
+
+    setError("");
+  }
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md: h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to log in.</p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value = {email}
+            onChange = {({target}) => setEmail(target.value)}
+            label = "Email Adress"
+            placeholder = "john.doe@example.com"
+            type = "text" />
+            <Input
+            value = {password}
+            onChange = {({target}) => setPassword(target.value)}
+            label = "Password"
+            placeholder = "Minimo 8 caracteres"
+            type = "password" />
+            {error &&  <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+            <button type='submit' className='btn-primary'>
+              LOGIN
+            </button>
+
+            <p className='text-[13px] text-slate-800 mt-3'>
+              No tienes cuenta?{" "}
+              <Link className='font-medium text-primary underline' to='/signUp'>
+                SignUp
+              </Link>
+            </p>
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
--- expense-tracker-app/src/pages/Auth/SignUp.jsx
+++ expense-tracker-app/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import {Link, useNavigate} from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [profilePic, setProfilePic] = useState(null);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+      e.preventDefault();
+      let profileImageUrl = "";
+      if(!validateEmail(email)) {
+        setError("Ingrese un correo valido");
+        return;
+      }
+  
+      if(!fullName){
+        setError("Ingrese una nombre valido");
+        return;
+      }
+
+      if(!password){
+        setError("Ingrese una contraseña valida");
+        return;
+      }
+  
+      setError("");
+    }
   return (
-    <div>SignUp</div>
+    <AuthLayout>
+      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Crea una cuenta</h3>
+        <p className='text-xs text-slate-700 mt[5px] mb-6'>
+          Registrate! :P
+        </p>
+
+
+        <form onSubmit={handleSignUp}>
+
+          <ProfilePhotoSelector image = {profilePic} setImage={setProfilePic} />
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <Input
+            value = {fullName}
+            onChange = {({target}) => setFullName(target.value)}
+            label = "Full Name"
+            placeholder = "Jhon"
+            type = "text" />
+            <Input
+            value = {email}
+            onChange = {({target}) => setEmail(target.value)}
+            label = "Email Adress"
+            placeholder = "john.doe@example.com"
+            type = "text" />
+            <div className='col-span-2'>
+            <Input
+            value = {password}
+            onChange = {({target}) => setPassword(target.value)}
+            label = "Password"
+            placeholder = "Minimo 8 caracteres"
+            type = "password" />
+            </div>
+          </div>
+
+          {error &&  <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          <button type='submit' className='btn-primary'>
+            SIGN UP
+          </button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Ya tienes cuenta? {" "}
+            <Link className='font-medium text-primary underline' to='/login'>
+              Login
+            </Link>
+          </p>
+
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
